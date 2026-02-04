3cc79d7fcb198912ad650b1cbefbc89af6a4ed22
abhishekcse08 <abhishekcse08@gmail.com>
2026-02-05 01:03:54 +0530

    first commit

---
+++ src/test/java/com/magnus/integration/MagnusDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnus.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnusDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
